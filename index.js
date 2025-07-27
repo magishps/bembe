611d6a6c7ec54f2f0163f9e3197c25cc8a782ff9
magis <magishps@gmail.com>
2025-07-28 02:37:27 +0300

    Добавил инициализацию команд

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-// Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 const { token } = require('./config.json');
-// Create a new client instance
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
+client.commands = new Collection();
+
+сonst foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for(const folder of commandFolders) {
+
+}
+
 client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+	console.log(`Готово! Залогинен как: ${readyClient.user.tag}`);
 });
 
-// Log in to Discord with your client's token
+// Логин в дискорде с помощью токена
 client.login(token);
